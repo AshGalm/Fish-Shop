39b8bebc5c2651b30b4a3b7f7d71b8a05d7915cb
Aeshah Qalmouz <outsiderash@gmail.com>
2024-11-10 21:08:40 +0200

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
-import { NotfindComponent } from './components/notfind/notfind.component';
+import { LoginComponent } from './login/login.component';
+import { NotfindComponent } from './notfind/notfind.component';
+import { ContactComponent } from './public/contact/contact.component';
+import { PricingComponent } from './public/pricing/pricing.component';
+import { ShoppingComponent } from './public/shopping/shopping.component';
 
 const routes: Routes = [
   {path:'',
   component:HomeComponent},
+  {
+    path:'login',
+    component:LoginComponent
+  },
+
+  {
+    path:'shopping',
+    component:ShoppingComponent
+  },
+
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+
+
+  {
+    path:'pricing',
+    component:PricingComponent
+  },
 
   {path:'**',
   component:NotfindComponent
-
   }
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap'
 import { HomeComponent } from './public/home/home.component';
 import { NavbarComponent } from './public/layout/navbar/navbar.component';
 import { FooterComponent } from './public/layout/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
-import { NotfindComponent } from './components/notfind/notfind.component';
+import { LoginComponent } from './login/login.component';
+import { NotfindComponent } from './notfind/notfind.component';
+import { ContactComponent } from './public/contact/contact.component';
+import { PricingComponent } from './public/pricing/pricing.component';
+import { ShoppingComponent } from './public/shopping/shopping.component';
 
 
 @NgModule({
@@ -17,7 +20,10 @@ import { NotfindComponent } from './components/notfind/notfind.component';
     LoginComponent,
     NavbarComponent,
     FooterComponent,
-    NotfindComponent
+    NotfindComponent,
+    ContactComponent,
+    PricingComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
